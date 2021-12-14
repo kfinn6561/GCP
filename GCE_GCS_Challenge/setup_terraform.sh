31d481076819f0deda7ced8d4b013bb255e453eb
kieran.finn <kieran.finn@bjss.com>
2021-12-14 11:34:04 +0000

    removed terraform files

--- GCE_GCS_Challenge/setup_terraform.sh
+++ GCE_GCS_Challenge/setup_terraform.sh
@@ -1,2 +1,3 @@
 gcloud auth login
 $ENV:GOOGLE_APPLICATION_CREDENTIALS=C:\Users\kiera\GCP_credentials/my-user-project-331814-terraform-sa.json
+$ENV:GOOGLE_APPLICATION_CREDENTIALS=C:\Users\kieran.finn\Documents\gcp_keys/my-user-project-331814-terraform-sa.json
